6273404f44c3516759b2a84669467e86adb54664
Vxer-Lee <2497057473@qq.com>
2021-06-23 23:39:27 +0800

    Doubana-Movie

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <AppHeader></AppHeader>
+    <keep-alive include = "home">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/Header"
+  export default{
+    components:{
+      AppHeader
+    },
+    created(){
+      //让页面走一下首页，触发Header中的router.beforeEach函数，不然头部会重复出现
+      this.$router.push("/")
+      //初始化购物车
+      this.$store.dispatch("initCar")
+    }
+  }
+</script>
+
+<style lang="scss">
+
+
+</style>
